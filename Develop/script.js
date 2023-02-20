1e9ec2a7291d501302690563470ea751ccb47ab0
Thomas Wray <twray95@gmail.com>
2023-02-20 10:44:26 -0700

    added dayjs time to header

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var today = dayjs();
+console.log(today);
 $(function () {
+  console.log(today);
+  $("#currentDay").text(today.format("dddd MMMM DD, HH:00"));
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
